8ce054895e8859522239521f21954f439a023654
Eray Guler <21058071@std.yildiz.edu.tr>
2024-03-25 22:46:47 +0300

    update

---
+++ 206. Reverse Linked List/solution.cpp
@@ -0,0 +1,39 @@
+//https://leetcode.com/problems/reverse-linked-list/description/?envType=daily-question&envId=2024-03-25
+#include <bits/stdc++.h>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while(curr!=NULL){
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+};
+int main(){
+    Solution s;
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    ListNode* ans = s.reverseList(head);
+    while(ans!=NULL){
+        cout<<ans->val<<" ";
+        ans = ans->next;
+    }
+    cout<<endl;
+    return 0;
+}
